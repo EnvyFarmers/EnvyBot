71c3332e47ae21b21b36f2189ca2d3938ec071fb
EnvyFarmers <enyfarmers@gmail.com>
2018-01-28 09:45:00 -0500

    Debug2

--- bot.js
+++ bot.js
@@ -55,9 +55,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                    var coinPrice = price[coin].USD;
                    logger.info(coinPrice)
                    var newMessage = '```==============  ' + coin + ' ============= \n'
-                   newMessage +=  'Price: ' + coinPrice.PRICE + '(' + coinPrice.CHANGEPCT24HOUR.toFixed(2) + ')' + '\n'
-                   newMessage +=  'Volume: ' + coinPrice.VOLUME24HOUR.toFixed(2) + '\n'
-                   newMessage +=  'MarketCap: ' + coinPrice.MKTCAP.toFixed(2) + '\n'
+                   // newMessage +=  'Price: ' + coinPrice.PRICE + '(' + coinPrice.CHANGEPCT24HOUR.toFixed(2) + ')' + '\n'
+                   // newMessage +=  'Volume: ' + coinPrice.VOLUME24HOUR.toFixed(2) + '\n'
+                   // newMessage +=  'MarketCap: ' + coinPrice.MKTCAP.toFixed(2) + '\n'
                    newMessage +=  '```'
                    bot.sendMessage({
                        to: channelID,
